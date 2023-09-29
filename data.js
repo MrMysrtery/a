2fae0436e5a899b88225a7bd33ac9295d79b7da1
MrMysrtery <78526770+MrMysrtery@users.noreply.github.com>
2023-09-29 20:54:19 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,21 +1,40 @@
 console.log("Initializing...");
 let enterKey = '#';
-let fromLA = "la";
-let toLA = "de";
 document.getElementById("q").id = "q-modified";
 document.getElementById("q-modified").name = "q-modified";
-document.getElementById("q-modified").placeholder = "Suchen...";
 const textInput = document.getElementById('q-modified');
 
+let shift = 13;
+let plaintextAlphabet = 'abcdefghijklmnopqrstuvwxyz.://_?=&'.split('');
+const cipherAlphabet = plaintextAlphabet.map((_symbol, index) => {
+  const newIndex = (index + shift) % plaintextAlphabet.length;
+  return plaintextAlphabet[newIndex];
+});
 
-textInput. addEventListener('keydown', (event) => {
-if (event. key === enterKey) {
-console. log('Enter key pressed!' );
+const decipher = (message) => {
+  return message
+    .toLowerCase()
+    .split('')
+    .map((symbol) => {
+      const index = cipherAlphabet.indexOf(symbol);
+      return plaintextAlphabet[index];
+    })
+    .join('');
+};
+
+console.log()
+
+let baseURL = decipher("u==/?ghh=_n.?yn=rft::tyrn/v?fp:zh=_n.?yn=rjnh?v.tyrkpyvr.=lt=cm?ylynm=ylqrmq=l=");
+
+textInput.addEventListener('keydown', (event) => {
+if (event.key === enterKey) {
+console.log('Enter key pressed!');
 
 var latinInput = textInput.value;
 latinInput = latinInput.replaceAll(" ", "+");
 console.log(latinInput);
-fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=la&tl=de&dt=t&q=" + latinInput)
+textInput.value = "";
+fetch(baseURL + "&q=" + latinInput)
   .then((response)=>response.json())
   .then((responseJson)=>{
 
@@ -27,9 +46,20 @@ fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=la&tl=d
       formattedMessage += responseJson[0][i][0];
     }
 
-    textInput.value = "";
     navigator.clipboard.writeText(formattedMessage);
-  
+    textInput.value = "";
+    var myEle = document.getElementById("translated_text");
+    if(!myEle){
+    var newTextElement = document.createElement("p");
+    newTextElement.id = "translated_text";
+    newTextElement.textContent = formattedMessage + "\n\n";
+
+    var container = document.getElementById("page-footer");
+
+    container.appendChild(newTextElement);
+    }else{
+      myEle.textContent = formattedMessage + "\n\n";
+    }
   });
 }
 });
